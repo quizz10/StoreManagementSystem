90633824e3985e2e1eaacbd1d8e4c663d56275e1
Ola Lindgård <ola.lindgard@iths.se>
2022-05-02 10:38:50 +0200

    Uppdaterat ShoppingCart med tillhörande klasser...

--- src/main/java/se/iths/storemanagementsystem/controller/ShoppingCartController.java
+++ src/main/java/se/iths/storemanagementsystem/controller/ShoppingCartController.java
@@ -1,89 +1,62 @@
-//package se.iths.storemanagementsystem.controller;
-//
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RestController;
-//import se.iths.storemanagementsystem.entity.Item;
-//import se.iths.storemanagementsystem.entity.ShoppingCart;
-//import se.iths.storemanagementsystem.service.ShoppingCartService;
-//import se.iths.storemanagementsystem.utils.JsonFormatter;
-//
-//import javax.inject.Inject;
-//import javax.ws.rs.*;
-//import javax.ws.rs.core.Response;
-//import java.util.Optional;
-//
-//@RestController
-//@RequestMapping("shoppingcart")
-//public class ShoppingCartController {
-//    ShoppingCartService shoppingCartService;
-//
-//    public ShoppingCartController(ShoppingCartService shoppingCartService) {
-//        this.shoppingCartService = shoppingCartService;
-//    }
-//
-//    @Path("")
-//    @POST
-//    public ResponseEntity<Optional<ShoppingCart>> createShoppingCart(ShoppingCart shoppingCart) {
-//        try {
-//            shoppingCartService.createShoppingCart(shoppingCart);
-//        } catch (Exception e) {
-//            throw new WebApplicationException(Response.status(Response.Status.CONFLICT).entity(new JsonFormatter(Response.Status.CONFLICT.getStatusCode(), "Could not create new shopping cart.l")).build());
-//        }
-//        return Response.ok(shoppingCart).build();
-//    }
-//
-//    @Path("{id}")
-//    @GET
-//    public ResponseEntity<Optional<Item>> getShoppingCartById(@PathParam("id") Long id) {
-//        Optional<ShoppingCart> foundCart;
-//        try {
-//            foundCart = shoppingCartService.findShoppingCartById(id);
-//        } catch (Exception e) {
-//            throw new WebApplicationException(Response.status(Response.Status.NOT_FOUND).entity(new JsonFormatter(Response.Status.NOT_FOUND.getStatusCode(), "Could not find a shopping cart with that ID.")).build());
-//        }
-//        return Response.ok(foundCart).build();
-//    }
-//
-//    @Path("link")
-//    @PATCH
-//    public ResponseEntity<Optional<Item>> linkItemToShoppingCart(@QueryParam("cartId") Long cartId, @QueryParam("itemId") Long itemId) {
-//        Optional<ShoppingCart> foundCart = shoppingCartService.findShoppingCartById(cartId);
-//        Optional<Item> foundItem = shoppingCartService.findItemById(itemId);
-//
-//        try {
-//            shoppingCartService.linkItemToShoppingCart(cartId, itemId);
-//        } catch (Exception e) {
-//            String exceptionHelper = null;
-//            if (foundCart == null) {
-//                exceptionHelper = "Shopping cart not found";
-//            }
-//            if (foundItem == null) {
-//                exceptionHelper = "Item not found";
-//            }
-//            throw new WebApplicationException(Response.status(Response.Status.NOT_FOUND).entity(new JsonFormatter(Response.Status.NOT_FOUND.getStatusCode(), exceptionHelper)).build());
-//        }
-//        return Response.ok(new JsonFormatter(Response.Status.OK.getStatusCode(), "Item with ID " + foundItem.getId() + " added to cart with ID " + foundCart.getId())).build();
-//    }
-//
-//    @Path("unlink")
-//    @PATCH
-//    public ResponseEntity<Optional<Item>> unlinkItemFromShoppingCart(@QueryParam("cartId") Long cartId, @QueryParam("itemId") Long itemId) {
-//        Optional<ShoppingCart> foundCart = shoppingCartService.findShoppingCartById(cartId);
-//        Optional<Item> foundItem = shoppingCartService.findItemById(itemId);
-//
-//        try {
-//            shoppingCartService.unlinkItemFromShoppingCart(cartId, itemId);
-//        } catch (Exception e) {
-//            String exceptionHelper = null;
-//            if (foundCart == null) {
-//                exceptionHelper = "Shopping cart not found";
-//            }
-//            if (foundItem == null) {
-//                exceptionHelper = "Item not found";
-//            }
-//            throw new WebApplicationException(Response.status(Response.Status.NOT_FOUND).entity(new JsonFormatter(Response.Status.NOT_FOUND.getStatusCode(), exceptionHelper)).build());
-//        }
-//        return Response.ok(new JsonFormatter(Response.Status.OK.getStatusCode(), "Item with ID " + foundItem.getId() + " removed from cart with ID " + foundCart.getId())).build();
-//    }
-//}
+package se.iths.storemanagementsystem.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import se.iths.storemanagementsystem.entity.Item;
+import se.iths.storemanagementsystem.entity.ShoppingCart;
+import se.iths.storemanagementsystem.service.ShoppingCartService;
+import se.iths.storemanagementsystem.utils.JsonFormatter;
+
+import javax.inject.Inject;
+import javax.ws.rs.*;
+import javax.ws.rs.core.Response;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("shoppingcart")
+public class ShoppingCartController {
+    ShoppingCartService shoppingCartService;
+
+    public ShoppingCartController() {}
+
+    public ShoppingCartController(ShoppingCartService shoppingCartService) {
+        this.shoppingCartService = shoppingCartService;
+    }
+
+    @PostMapping("")
+    public ResponseEntity<Optional<ShoppingCart>> createShoppingCart(@RequestBody ShoppingCart shoppingCart) {
+        Optional<ShoppingCart> newShoppingCart;
+            newShoppingCart = shoppingCartService.createShoppingCart(shoppingCart);
+        return new ResponseEntity<>(newShoppingCart, HttpStatus.CREATED);
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<Optional<ShoppingCart>> getShoppingCartById(@PathVariable Long id) {
+        Optional<ShoppingCart> foundCart = shoppingCartService.findShoppingCartById(id);
+
+        return new ResponseEntity<>(foundCart, HttpStatus.FOUND);
+    }
+
+    @PatchMapping("link")
+    public ResponseEntity<Optional<ShoppingCart>> linkItemToShoppingCart(@RequestParam("cartId") Long cartId, @RequestParam("itemId") Long itemId) {
+        Optional<ShoppingCart> foundCart = shoppingCartService.findShoppingCartById(cartId);
+        Optional<Item> foundItem = shoppingCartService.findItemById(itemId);
+
+            Optional<ShoppingCart> cart = shoppingCartService.linkItemToShoppingCart(cartId, itemId);
+
+            return new ResponseEntity<>(cart, HttpStatus.CREATED);
+    }
+
+    @Path("unlink")
+    @PATCH
+    public ResponseEntity<Optional<ShoppingCart>> unlinkItemFromShoppingCart(@QueryParam("cartId") Long cartId, @QueryParam("itemId") Long itemId) {
+        Optional<ShoppingCart> foundCart = shoppingCartService.findShoppingCartById(cartId);
+        Optional<Item> foundItem = shoppingCartService.findItemById(itemId);
+
+
+            Optional<ShoppingCart> cart = shoppingCartService.unlinkItemFromShoppingCart(cartId, itemId);
+
+        return new ResponseEntity<>(cart, HttpStatus.OK);
+    }
+}
--- src/main/java/se/iths/storemanagementsystem/service/ShoppingCartService.java
+++ src/main/java/se/iths/storemanagementsystem/service/ShoppingCartService.java
@@ -1,48 +1,61 @@
-//package se.iths.storemanagementsystem.service;
-//
-//import org.springframework.stereotype.Service;
-//import se.iths.storemanagementsystem.entity.Item;
-//import se.iths.storemanagementsystem.entity.ShoppingCart;
-//import se.iths.storemanagementsystem.repository.ItemRepository;
-//import se.iths.storemanagementsystem.repository.ShoppingCartRepository;
-//
-//import javax.persistence.EntityNotFoundException;
-//import java.util.Optional;
-//
-//@Service
-//public class ShoppingCartService {
-//
-//    private final ShoppingCartRepository shoppingCartRepository;
-//    private final ItemRepository itemRepository;
-//
-//    public ShoppingCartService(ShoppingCartRepository shoppingCartRepository, ItemRepository itemRepository) {
-//        this.shoppingCartRepository = shoppingCartRepository;
-//        this.itemRepository = itemRepository;
-//    }
-//
-//    public void createShoppingCart(ShoppingCart shoppingCart) {
-//        .save(shoppingCart);
-//    }
-//
-//    public Optional<ShoppingCart> findShoppingCartById(Long id) {
-//        return Optional.ofNullable(shoppingCartRepository.findById(id).orElseThrow(EntityNotFoundException::new));
-//    }
-//
-//    public Optional<Item> findItemById(Long id) {
-//        return Optional.ofNullable(itemRepository.findById(id).orElseThrow(EntityNotFoundException::new));
-//    }
-//
-//    public void linkItemToShoppingCart(Long cartId, Long itemId) {
-//        ShoppingCart foundCart = .find(ShoppingCart.class, cartId);
-//        Item foundItem = .find(Item.class, itemId);
-//
-//        foundCart.addItem(foundItem);
-//    }
-//
-//    public void unlinkItemFromShoppingCart(Long cartId, Long itemId) {
-//        ShoppingCart foundCart = .find(ShoppingCart.class, cartId);
-//        Item foundItem = .find(Item.class, itemId);
-//
-//        foundCart.removeItem(foundItem);
-//    }
-//}
+package se.iths.storemanagementsystem.service;
+
+import org.springframework.stereotype.Service;
+import se.iths.storemanagementsystem.entity.Item;
+import se.iths.storemanagementsystem.entity.ShoppingCart;
+import se.iths.storemanagementsystem.repository.ItemRepository;
+import se.iths.storemanagementsystem.repository.ShoppingCartRepository;
+
+import javax.persistence.EntityNotFoundException;
+import java.util.Optional;
+
+@Service
+public class ShoppingCartService {
+
+    private final ShoppingCartRepository shoppingCartRepository;
+    private final ItemRepository itemRepository;
+
+    public ShoppingCartService(ShoppingCartRepository shoppingCartRepository, ItemRepository itemRepository) {
+        this.shoppingCartRepository = shoppingCartRepository;
+        this.itemRepository = itemRepository;
+    }
+
+    public Optional<ShoppingCart> createShoppingCart(ShoppingCart shoppingCart) {
+        ShoppingCart cart = shoppingCartRepository.save(shoppingCart);
+        return Optional.ofNullable(cart);
+    }
+
+    public void deleteShoppingCart(Long id) {
+        Optional<ShoppingCart> foundCart = shoppingCartRepository.findById(id);
+        for(Item item : foundCart.get().getItems()) {
+            foundCart.get().removeItem(item);
+        }
+        foundCart.get().removeUser(foundCart.get().getUser());
+    }
+
+    public Optional<ShoppingCart> findShoppingCartById(Long id) {
+        return Optional.ofNullable(shoppingCartRepository.findById(id).orElseThrow(EntityNotFoundException::new));
+    }
+
+    public Optional<Item> findItemById(Long id) {
+        return Optional.ofNullable(itemRepository.findById(id).orElseThrow(EntityNotFoundException::new));
+    }
+
+    public Optional<ShoppingCart> linkItemToShoppingCart(Long cartId, Long itemId) {
+        Optional<ShoppingCart> foundCart = shoppingCartRepository.findById(cartId);
+        Optional<Item> foundItem = itemRepository.findById(itemId);
+
+            foundCart.get().addItem(Optional.of(foundItem.get()));
+            return foundCart;
+
+    }
+
+    public Optional<ShoppingCart> unlinkItemFromShoppingCart(Long cartId, Long itemId) {
+        Optional<ShoppingCart> foundCart = shoppingCartRepository.findById(cartId);
+        Optional<Item> foundItem = itemRepository.findById(itemId);
+
+            foundCart.get().removeItem(foundItem.get());
+            return foundCart;
+
+    }
+}
